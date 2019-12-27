68e5c915647c23d5ef74a694a66a7e7534128007
中山 陽介 <bp-ynakayama@kdl.co.jp>
2019-12-27 19:35:59 +0900

    Initial Commit

---
+++ NewsAppRxSwiftMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppRxSwiftMVVM
+//
+//  Created by 中山 陽介 on 2019/12/27.
+//  Copyright © 2019 中山 陽介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
